f4521310c9a00f8ed0e8554ff2ae56abce8c59f0
DuckSeulPang <138849786+DUCKSEUL@users.noreply.github.com>
2026-09-18 07:41:06 +0900

    [Passed] 동전이 있는 위치 (50ms, 16MB)

--- trail1/동전이 있는 위치/Where-coins-are-located.py
+++ trail1/동전이 있는 위치/Where-coins-are-located.py
@@ -1,18 +1,13 @@
-arr = []
 N, M = map(int, input().split())
-for _ in range(M):
-    arr.append(list(map(int, input().split())))
 
 cat = [
     [0 for _ in range(N)]
     for _ in range(N)
 ]
 
-for i in range(N):
-    for j in range(N):
-        for k in range(M):
-            if (i+1 == arr[k][0]) and (j+1 == arr[k][1]):
-                cat[i][j] = 1
+for _ in range(M):
+    x, y = map(int, input().split())
+    cat[x-1][y-1] = 1
 
 for i in range(N):
     for j in range(N):
